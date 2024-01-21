715e233210057e4aecd757c589a64c64fd4e0fd6
Fischer Bordwell <fbordwel@purdue.edu>
2024-01-20 20:07:01 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "lyahfgg";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = inputs: let
+    overlay = final: prev: {
+      haskell =
+        prev.haskell
+        // {
+          packageOverrides = hfinal: hprev:
+            prev.haskell.packageOverrides hfinal hprev
+            // {
+              lyahfgg = hfinal.callCabal2nix "lyahfgg" ./. {};
+            };
+        };
+      lyahfgg = final.haskell.lib.compose.justStaticExecutables final.haskellPackages.lyahfgg;
+    };
+    perSystem = system: let
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [overlay];
+      };
+      hspkgs = pkgs.haskellPackages;
+    in {
+      devShell = hspkgs.shellFor {
+        withHoogle = true;
+        packages = p: [p.lyahfgg];
+        buildInputs = [
+          hspkgs.cabal-install
+          hspkgs.ghcid
+          hspkgs.ormolu
+          hspkgs.hlint
+        ];
+      };
+      defaultPackage = pkgs.lyahfgg;
+    };
+  in
+    {inherit overlay;} // inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
