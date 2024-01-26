1f79bef9ca987d22abc0924bddfcedbf56844432
Fischer Bordwell <fbordwel@purdue.edu>
2024-01-25 23:08:49 -0800

    State monad

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,7 +1,7 @@
 module MyLib where
 
-import Control.Monad.Instances
 import Control.Monad.Writer
+import Control.Monad.State
 import Data.Char
 import qualified Data.Foldable as F
 import Data.List
@@ -161,3 +161,24 @@ addStuff = do
   a <- (* 2)
   b <- (+ 10)
   return (a + b)
+
+type Stack = [Int]
+
+pop :: Stack -> (Int, Stack)
+pop (x : xs) = (x, xs)
+
+push :: Int -> Stack -> ((), Stack)
+push a xs = ((), a : xs)
+
+stackManip :: Stack -> (Int, Stack)
+stackManip stack =
+  let
+    ((), newStack1) = push 3 stack
+    (_ , newStack2) = pop newStack1
+   in pop newStack2
+
+pop' :: State Stack Int
+pop' = state $ \(x:xs) -> (x, xs)
+
+push' :: Int -> State Stack ()
+push' a = state $ \xs -> ((), a:xs)
